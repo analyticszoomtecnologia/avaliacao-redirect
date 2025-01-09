072e1000853a93444564dc676e9fb7876de23bc5
analyticszoomtecnologia <analyticsdatazoom@gmail.com>
2025-01-08 23:50:51 -0300

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -71,7 +71,7 @@ def buscar_colaboradores():
         ON
             fz.Diretor_Gestor = lt.Nome
         WHERE
-            lt.id_emp = id;
+            lt.id_emp = user_id;
     """)
     colaboradores = cursor.fetchall()
     cursor.close()
