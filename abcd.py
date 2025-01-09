630d23e5009ac7f9d7dd734d64105def03e109c6
analyticszoomtecnologia <analyticsdatazoom@gmail.com>
2025-01-08 21:57:06 -0300

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -75,7 +75,7 @@ def buscar_colaboradores():
         WHERE Diretor_Gestor = (
             SELECT Nome
             FROM datalake.silver_pny.func_zoom
-            WHERE Diretor_Gestor = 'Grasiele Boff'
+            WHERE Diretor_Gestor = 'Grasiele Bof'
         )
     """)
 
